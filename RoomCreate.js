9d127ac9ce55ef4934bb84ca5a660b5052eb99f7
jdiperla <jdiperla@gmail.com>
2015-08-11 23:24:09 -0400

    Update RoomCreate.js
    Converted the room into an Object for more a much better room loading script.

--- RoomCreate.js
+++ RoomCreate.js
@@ -1,12 +1,17 @@
 //This file has all the data needed to load up a room.
 
 //Initiates the canvas and resolution size of the background. 
-function SetRoomSize(XRes, YRes) {
-      document.getElementById("StartRoomLoad").innerHTML += "<canvas id='RoomBackground' width='" + XRes + "' height='" + YRes + "'></canvas>";      }
-      
-      //This next function will load the initial room image. 
-function LoadRoomBackground(RmImg) {
-var canvas = document.getElementById('RoomBackground');
+var GameRoom = {
+    RoomName: "",
+    RoomDesc : "",
+    id       : 0,
+    XRes     : 320,
+    YRes     : 240,
+    SetRoomSize : function(){
+           document.getElementById("StartRoomLoad").innerHTML += "<canvas id='RoomBackground' width='" + XRes + "' height='" + YRes + "'></canvas>";      
+                            },
+    LoadRoomBackground : function(){
+          var canvas = document.getElementById('RoomBackground');
         var context = canvas.getContext('2d');
 
         // load image from data url
@@ -17,8 +22,10 @@ var canvas = document.getElementById('RoomBackground');
 
         imageObj.src = RmImg;
       }
+    };                            
+      
+ 
 
-function RmBgWalkPan() {
-//This will pan the room for scrolling rooms when the player walks around. This happens at slow intervals using context.drawImage
-//Have to figure out how to scroll other properties of the room with the panning such as characters, objects and hotspots, etc...
-}
+
+
+

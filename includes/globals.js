70a759dc506570613a976b47b1811d3deae632a8
jdiperla <jdiperla@gmail.com>
2015-08-13 22:10:21 -0400

    Update globals.js
    Added basic Globals

--- includes/globals.js
+++ includes/globals.js
@@ -1,1 +1,6 @@
+//Global variables
+var AddtoInventory=new Array();
+var GameActions=new Array("Look At","Take","Combine","Speak","Take Off","Put on","Punch","Kick","Lick","Taste","Smell","Push","Pull","Walk to","Talk to","Pick up", "Drop","Call",Fight","Read","Open","Close","What is","Turn on", "Turn off","Switch","Travel");
+var SpecialGameActions = new Array("Use","Give");
+
 

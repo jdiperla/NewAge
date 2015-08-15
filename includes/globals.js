47c012984dc9c86ec3e91169e4e524fe765c9662
jdiperla <jdiperla@gmail.com>
2015-08-15 09:31:57 -0400

    Update globals.js

--- includes/globals.js
+++ includes/globals.js
@@ -1,4 +1,4 @@
-//Global variables
+//Global variables, definitions and functions
 
 //These are for initial game settings
 var GameCanvas = "";
@@ -15,4 +15,11 @@ var PreviousCursorMode = "Look";
 var SierraCurModeCycle = new Array(); // This is the area used to cycle through the cursor modes when right clicking, like in Sierra games
 var SierraCurModeCurrent = 0; //This will define the Current cursor mode if Sierra Style Cursor Mode is chosen.
 var SierraCurModeMax = 4; //The max number of cycles to go through to get at zero, starting from zero and counting up
+
+//Defining the objects, characters and other functions.
+
+function GameSprites(id, SpriteArray){
+  this.SpriteArray = [SpriteArray];
+  this.id = id;
+}
 

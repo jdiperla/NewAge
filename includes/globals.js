d02b70152ce7ba42191d9dcebff4958bfe48ae37
jdiperla <jdiperla@gmail.com>
2015-08-14 20:09:22 -0400

    Update globals.js
    Adding more Global Vars

--- includes/globals.js
+++ includes/globals.js
@@ -1,6 +1,18 @@
 //Global variables
+
+//These are for initial game settings
+var GameCanvas = "";
+var XRes = "320";
+var YRes = "240";
+
+//These variables are for the games interactions, GUI and inventory
 var AddtoInventory=new Array();
 var GameActions=new Array("Look At","Take","Combine","Speak","Take Off","Put on","Punch","Kick","Lick","Taste","Smell","Push","Pull","Walk to","Talk to","Pick up", "Drop","Call",Fight","Read","Open","Close","What is","Turn on", "Turn off","Switch","Travel");
 var SpecialGameActions = new Array("Use","Give");
-
+var CurrentCursorMode = "Walk to";
+var DefaultCursorMode = "Walk to";
+var PreviousCursorMode = "Look";
+var SierraCurModeCycle = new Array(); // This is the area used to cycle through the cursor modes when right clicking, like in Sierra games
+var SierraCurModeCurrent = 0; //This will define the Current cursor mode if Sierra Style Cursor Mode is chosen.
+var SierraCurModeMax = 4; //The max number of cycles to go through to get at zero, starting from zero and counting up
 
